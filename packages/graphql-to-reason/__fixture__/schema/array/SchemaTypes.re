941df76c511dcc8f1da702c0c59a84ed26a1d14a
sainthkh <sainthkh@naver.com>
2019-02-17 20:56:55 +0900

    Generate array types.

---
+++ packages/graphql-to-reason/__fixture__/schema/array/SchemaTypes.re
@@ -0,0 +1,6 @@
+type queryResponse = {
+  i1: option(array(option(int))),
+  i2: array(option(int)),
+  i3: option(array(int)),
+  i4: array(int),
+};
--- packages/graphql-to-reason/src/generator.js
+++ packages/graphql-to-reason/src/generator.js
@@ -15,10 +15,34 @@ function generateTypeListFromSchema(ast) {
             option: true,
           }
         case "NonNullType": 
+          if(field.type.type.kind != "ListType") {
+            return {
+              name: field.name.value,
+              type: field.type.type.name.value,
+              option: false,
+            }
+          } else {
+            let nullable = field.type.type.type.kind == "NamedType";
+            return {
+              name: field.name.value, 
+              type: nullable
+                ? field.type.type.type.name.value
+                : field.type.type.type.type.name.value,
+              option: false,
+              array: true,
+              contentOption: nullable,
+            }
+          }
+        case "ListType":
+          let nullableType = field.type.type.kind == "NamedType";
           return {
-            name: field.name.value, 
-            type: field.type.type.name.value,  
-            option: false,
+            name: field.name.value,
+            type: nullableType
+              ? field.type.type.name.value
+              : field.type.type.type.name.value,
+            option: true,
+            array: true,
+            contentOption: nullableType,
           }
         }
       })
@@ -94,6 +118,13 @@ function decodeTypeName(field) {
 
   let typeName = typeNames[field.type];
   typeName = typeName ? typeName : lowerTheFirstCharacter(field.type);
+  typeName = field.contentOption
+    ? `option(${typeName})`
+    : typeName;
+  
+  typeName = field.array
+    ? `array(${typeName})`
+    : typeName;
 
   return field.option? `option(${typeName})` : typeName;
 }
