c1c0d2f4c88306ebf6f6ad5f81b5b6a0b38d91d3
sainthkh <sainthkh@naver.com>
2019-02-20 10:20:53 +0900

    Fixed object types to have selection name prefix.

--- packages/graphql-to-reason/src/client.js
+++ packages/graphql-to-reason/src/client.js
@@ -2,32 +2,39 @@ const {
   generateTypeCode,
   commentOnTop,
   childTypes,
+  isScalar,
 } = require('./util')
 
 function generateTypeListFromQuery(ast) {
   //console.log(require('ast-pretty-print')(ast));
 
   let types = {};
-  extractType(types, ast);
+  extractType(types, ast, []);
 
   let typeList = childTypes(types, types["Query"]);
   return typeList;
 }
 
-function extractType(types, ast) {
+function extractType(types, ast, selectionNames) {
   let fields = ast.selections.map(selection => {
     if(selection.kind == "LinkedField") {
-      extractType(types, selection);
+      extractType(types, selection, [...selectionNames, selection.name]);
     }
-    let {option, typeName} = interpretType(selection.type)
+
+    let {option, typeName, array, contentOption} = interpretType(selection.type)
     return {
       name: selection.name,
-      type: typeName,
+      type: isScalar(typeName)
+        ? typeName
+        : [...selectionNames, selection.name, typeName].join('_'),
       option,
+      array, 
+      contentOption,
     }
   })
   
-  let name = ast.type.name ? ast.type.name : ast.type.ofType.name;
+  let {typeName} = interpretType(ast.type);
+  let name = [...selectionNames, typeName].join('_');
   types[name] = {
     name,
     fields,
@@ -37,14 +44,41 @@ function extractType(types, ast) {
 function interpretType(type) {
   let typeName = '' + type;
   if(typeName[typeName.length - 1] == '!') {
-    return {
-      option: false,
-      typeName: typeName.substring(0, typeName.length - 1),
+    if(typeName[0] == '[') {
+      let contentOption = typeName[typeName.length - 3] != '!';
+      return {
+        option: false,
+        typeName: typeName.substring(
+          1, 
+          typeName.length - (contentOption ? 2 : 3)
+        ),
+        array: true,
+        contentOption,
+      }
+    } else {
+      // Type!
+      return {
+        option: false,
+        typeName: typeName.substring(0, typeName.length - 1),
+      }
     }
   } else {
-    return {
-      option: true,
-      typeName,
+    if(typeName[0] == '[') {
+      let contentOption = typeName[typeName.length - 2] != '!';
+      return {
+        option: true,
+        typeName: typeName.substring(
+          1, 
+          typeName.length - (contentOption ? 1 : 2)
+        ),
+        array: true,
+        contentOption,
+      }
+    } else {
+      return {
+        option: true,
+        typeName,
+      }
     }
   }
 }
